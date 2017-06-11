04a20be7311c90515479dc7439551e6da6dd2738
Danish Khakwani <danish.khakwani@yahoo.com>
2017-06-11 15:31:09 -0700

    Added hello world route; implemented start scrupt

---
+++ flask_app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+from flask_app.settings import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+db = SQLAlchemy(app)
+api = Api(app)
+
+from flask_app import views
